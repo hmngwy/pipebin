38dfb3b34311f0b21aa0fb6f9ac44c32069c5f51
hmngwy <cp.ambrosio@gmail.com>
2017-05-18 10:58:15 +0800

    added gpg action filter

--- http_server.py
+++ http_server.py
@@ -4,6 +4,8 @@ import traceback
 import sys
 import shutil
 import helpers
+import os
+import datetime
 
 app = Flask(__name__)
 
@@ -13,32 +15,35 @@ def home():
     message += 'a bytestring safe terminal pastebin clone\n'
     message += '\n'
     message += '# send data\n'
-    message += '$ echo "simple" | nc pipeb.in 10000\n'
+    message += '$ echo "simple" | nc pipebin.org 10000\n'
     message += '\n'
     message += '# send encrypted data as binary\n'
-    message += '$ ls -la | openssl enc -aes-256-cbc | nc pipeb.in 10000\n'
+    message += '$ ls -la | openssl enc -aes-256-cbc | nc pipebin.org 10000\n'
     return Response(message, mimetype='text/plain')
 
 @app.route("/<slug>")
 def read(slug):
-    with open(path_gen(slug), 'rb') as file:
-        message = file.read();
-        is_binary = helpers.is_binary_string(message)
-    return Response(message, mimetype='octet-stream' if is_binary else 'text/plain')
+    if os.path.isfile(helpers.path_gen(slug)):
+        with open(helpers.path_gen(slug), 'rb') as file:
+            message = file.read();
+            is_binary = helpers.is_binary_string(message)
+        return Response(message, mimetype='octet-stream' if is_binary else 'text/plain')
+    else:
+        abort(404)
 
-@app.route("/gpg:<keyserver>:<keyid>/<slug>")
-def decrypt(slug, keyserver, keyid):
-    keyserver = config.keyservers[keyserver]
-    gpg, gpg_homedir = create_gpg()
+@app.route("/gpg:<keyserver>:<action>:<keyid>/<slug>")
+def decrypt(slug, keyserver, action, keyid):
+    keyserver_full = config.keyservers[keyserver]
+    gpg, gpg_homedir = helpers.create_gpg()
 
     try:
-        import_result = gpg.recv_keys(keyserver, keyid)
+        import_result = gpg.recv_keys(keyserver_full, keyid)
     except Exception as e:
-        print(traceback.format_exception(*sys.exc_info()))
-        abort(500)
+        print(str(e))
+        abort(404)
 
     if len(import_result.fingerprints)>0:
-        with open(path_gen(slug), "rb") as out:
+        with open(helpers.path_gen(slug), "rb") as out:
             decrypted_data = gpg.decrypt(out.read(), always_trust=True)
             delete_result = gpg.delete_keys(import_result.fingerprints[0])
             shutil.rmtree(gpg_homedir)
@@ -47,12 +52,21 @@ def decrypt(slug, keyserver, keyid):
             print(decrypted_data.ok)
 
             if decrypted_data.ok or decrypted_data.status == 'signature valid':
-                message = str(decrypted_data)
-                return Response(message, mimetype='text/plain')
+                if action == 'decrypt':
+                    message = str(decrypted_data)
+                    return Response(message, mimetype='text/plain')
+                elif action == 'verify':
+                    message = "{0} {1}\nFingerprint: {2}\nTrust Level: {3}\nTrust Text: {4}\nSignature ID: {5}\n".format(decrypted_data.username, decrypted_data.key_id, decrypted_data.fingerprint, decrypted_data.trust_level, decrypted_data.trust_text, decrypted_data.signature_id)
+                    #message += "\nWithout Signature Data: http://{0}/gpg:{1}:decrypt:{2}/{3}\n".format(config.domain, keyserver, keyid, slug)
+                    message += "\n----- Verification Time: " + str(datetime.datetime.now()).split('.')[0] + " -----\n"
+                    message += "\n" + str(decrypted_data)
+                    return Response(message, mimetype='text/plain')
+                else:
+                    abort(404)
             else:
                 abort(404)
     else:
-        print('key does not exist')
+        print('Key import failed')
         abort(404)
 
 if __name__ == "__main__":
